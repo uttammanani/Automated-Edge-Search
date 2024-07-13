99eab4315cb5ac32ac0755a5c2d798c06b3df44b
uttammanani <uttammanani@gmail.com>
2024-07-14 03:48:06 +0530

    Updated script to open Edge via Windows Search button

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
+# import subprocess
+# import time
+# import random
+# import string
+# import os
+# import pyautogui
+
+# # Function to generate a random query
+# def generate_random_query(length=10):
+#     letters = string.ascii_lowercase
+#     return ''.join(random.choice(letters) for _ in range(length))
+
+# # Path to Microsoft Edge executable
+# edge_path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
+
+# # URL of Microsoft Bing
+# bing_url = "https://www.bing.com/search?q="
+
+# # Open Microsoft Edge
+# subprocess.Popen([edge_path])
+
+# # Allow some time for the browser to open
+# time.sleep(5)
+
+# # Perform 30 searches with random queries
+# for _ in range(30):
+#     random_query = generate_random_query()
+#     search_url = bing_url + random_query
+#     subprocess.Popen([edge_path, search_url])
+#     time.sleep(10)
+
+# # Close all tabs using Ctrl+W
+# for _ in range(31):
+#     pyautogui.hotkey('ctrl', 'w')
+#     time.sleep(1)
+
+# # Close Microsoft Edge
+# os.system("taskkill /IM msedge.exe /F")
+
 import subprocess
 import time
 import random
@@ -13,24 +52,32 @@ def generate_random_query(length=10):
 # Path to Microsoft Edge executable
 edge_path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
 
-# URL of Microsoft Bing
-bing_url = "https://www.bing.com/search?q="
-
 # Open Microsoft Edge
 subprocess.Popen([edge_path])
 
 # Allow some time for the browser to open
 time.sleep(5)
 
-# Perform 30 searches with random queries
+# Perform 30 searches with random queries using the Windows button for input
 for _ in range(30):
     random_query = generate_random_query()
-    search_url = bing_url + random_query
-    subprocess.Popen([edge_path, search_url])
+    
+    # Press Windows key to open search
+    pyautogui.hotkey('win', 's')
+    time.sleep(1)
+    
+    # Type the random query
+    pyautogui.write(random_query)
+    time.sleep(1)
+    
+    # Press Enter to perform the search
+    pyautogui.press('enter')
+    
+    # Allow some time for the search to complete and open a new tab
     time.sleep(30)
 
 # Close all tabs using Ctrl+W
-for _ in range(31):
+for _ in range(30):
     pyautogui.hotkey('ctrl', 'w')
     time.sleep(1)
 
